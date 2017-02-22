74f2439983b3dbdbd64784371714cfc19116042d
edwinwells <edwin.wallace@yandex.com>
2017-02-22 16:34:38 -0500

    Test & name.erb
    are now syntactically modified to coincide.  Eight assertions now run with 0 failures, 0 errors.

--- Test_new_personal_details.rb
+++ Test_new_personal_details.rb
@@ -1,6 +1,6 @@
 require 'minitest/autorun'
 require 'rack/test'
-require_relative '../app.rb'
+require_relative 'app.rb'
 class TestApp < Minitest::Test 
     include Rack::Test::Methods
     def app
